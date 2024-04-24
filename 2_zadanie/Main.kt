d0e5617869c6d02a0527c5ca57adb07c1a766224
Njkita <117151719+Njkita@users.noreply.github.com>
2024-04-25 01:15:27 +0300

    Update Main.kt

--- 2_zadanie/Main.kt
+++ 2_zadanie/Main.kt
@@ -1,33 +1,35 @@
+import java.net.URL
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.URL
 
 fun main() {
-    val url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%8E%D0%B7_%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%D1%81%D0%BA%D0%B8%D1%85_%D0%A1%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D1%85_%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA"
-    val content = fetchContent(url)
-    val links = extractLinks(content)
-    links.forEach { println(it) }
+    val url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
+    val Gettinglink = Geturl(url)
+    val links = derivelinks(Gettinglink)
+    for (link in links) {
+        println(link)
+    }
 }
 
-fun fetchContent(urlString: String): String {
-    val url = URL(urlString)
-    val connection = url.openConnection()
-    val content = StringBuilder()
+fun Geturl (Stringurl: String): String {
+    val url = URL(Stringurl)
+    val compound = url.openConnection()
+    val allline = StringBuilder()
 
-    BufferedReader(InputStreamReader(connection.getInputStream())).use { reader ->
+    BufferedReader(InputStreamReader(compound.getInputStream())).use { reader ->
         var line: String?
         while (reader.readLine().also { line = it } != null) {
-            content.append(line)
+            allline.append(line)
         }
     }
 
-    return content.toString()
+    return allline.toString()
 }
 
-fun extractLinks(html: String): List<String> {
-    val linkRegex = "<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"".toRegex()
-    return linkRegex.findAll(html).map { matchResult ->
-        val link = matchResult.groups[1]?.value ?: ""
-        if (link.startsWith("/")) "https://en.wikipedia.org$link" else link
+fun derivelinks(html: String): List<String> {
+    val linkRegular = "<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"".toRegex()
+    return linkRegular.findAll(html).map { output ->
+        val link = output.groups[1]?.value ?: ""
+        if (link.startsWith("/")) "https://en.wikipedia.org" + link else link
     }.toList()
 }
