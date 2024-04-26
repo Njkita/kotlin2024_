cb69885b226a20f2a7ba86727e7dc81a04d82b6b
Njkita <117151719+Njkita@users.noreply.github.com>
2024-04-26 17:09:40 +0300

    Update Main.kt

--- 2_zadanie/Main.kt
+++ 2_zadanie/Main.kt
@@ -2,38 +2,37 @@ import java.net.URL
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun main() {
-    val url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
-    val Gettinglink = Geturl(url)
-    val links = derivelinks(Gettinglink)
-    for (link in links) {
-        println(link)
-    }
-}
-
 fun Geturl (Stringurl: String): String {
     val url = URL(Stringurl)
     val compound = url.openConnection()
     val allline = StringBuilder()
 
-    //считывание данных с сервера, библиотечная функция
     BufferedReader(InputStreamReader(compound.getInputStream())).use { reader ->
         var line: String?
         while (reader.readLine().also { line = it } != null) {
             allline.append(line)
         }
     }
+
     return allline.toString()
 }
 
-//извлечение гиперссылок из строки
 fun derivelinks(html: String): List<String> {
-    //Фактический URL-адрес: регулярное выражение для ссылок для нахождения тегов <a> и их атрибутов href в HTML.:
+    //Регулярное выражение для ссылок:
     val linkRegular = "<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"".toRegex()
-    //поиск совпадений
-    return linkRegular.findAll(html).map { output ->
-        val link = output.groups[1]?.value ?: ""
-        //преобразование относительного URL-адреса в абсолютный
+    val htmllink = linkRegular.findAll(html)
+    return htmllink.map { output ->
+        val link = output.groupValues[1]
+        if (link.startsWith("#")) Unit
         if (link.startsWith("/")) "https://en.wikipedia.org" + link else link
     }.toList()
+}
+
+fun main() {
+    val url1 = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
+    val text = Geturl(url1)
+    val links = derivelinks(text)
+    for (link in links) {
+        println(link)
+    }
 }
